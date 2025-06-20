a35b307dee62d966ef53367acd036045b8715b64
R-chocolate <94609858+R-chocolate@users.noreply.github.com>
2025-06-20 15:07:15 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Dice3DApp from './Dice3DApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Dice3DApp />
+  </React.StrictMode>
+);
